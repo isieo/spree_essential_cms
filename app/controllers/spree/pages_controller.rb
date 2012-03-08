f2b2112d8665c843154c722fe6e61e570aa9578d
Calvin Tee <isieox+github@gmail.com>
2012-03-08 22:56:42 +0800

    allow pages to access searchers and products

--- app/controllers/spree/pages_controller.rb
+++ app/controllers/spree/pages_controller.rb
@@ -2,6 +2,8 @@ class Spree::PagesController < Spree::BaseController
 
   def show
     @page = current_page
+    @searcher = Spree::Config.searcher_class.new(params)
+    @products = @searcher.retrieve_products
     raise ActionController::RoutingError.new("No route matches [GET] #{request.fullpath}") if @page.nil? 
     if @page.root?
       @posts    = Spree::Post.live.limit(5)    if SpreeEssentials.has?(:blog)
